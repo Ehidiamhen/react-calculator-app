cb3bde0855043a4c3589ac99b75faa97fb0b6b19
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-10-08 23:23:39 +0100

    Initial Commit

---
+++ src/OperationButton.js
@@ -0,0 +1,3 @@
+export default function OperationButton({dispatch, operation}) {
+    return <button className="operation"onClick={() => dispatch({type: 'add-operation', payload: {operation}})}>{operation}</button>
+}
